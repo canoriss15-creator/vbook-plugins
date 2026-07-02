61ded2804abff90076b2fb967aa2d127d792ee04
canoriss15-creator <canoriss15@gmail.com>
2026-07-02 20:17:04 +0700

    Add files via upload

---
+++ source.js
@@ -0,0 +1,86 @@
+var BASE_URL = "https://hentainexus.com";
+
+function getHome(page) {
+    if (!page) page = 1;
+    var response = Fetch.get(BASE_URL + "/?page=" + page);
+    if (response.status === 200) {
+        var doc = Html.parse(response.body);
+        var listHtml = doc.select(".column .card"); 
+        var listBook = [];
+        
+        for (var i = 0; i < listHtml.size(); i++) {
+            var element = listHtml.get(i);
+            listBook.push({
+                name: element.select(".card-header-title a").text(),
+                cover: element.select(".card-image img").attr("src"),
+                link: element.select(".card-header-title a").attr("href"),
+                host: BASE_URL
+            });
+        }
+        return Response.success(listBook);
+    }
+    return Response.error("Lỗi kết nối");
+}
+
+function search(keyword, page) {
+    if (!page) page = 1;
+    var response = Fetch.get(BASE_URL + "/search?q=" + encodeURIComponent(keyword) + "&page=" + page);
+    if (response.status === 200) {
+        var doc = Html.parse(response.body);
+        var listHtml = doc.select(".column .card");
+        var listBook = [];
+        
+        for (var i = 0; i < listHtml.size(); i++) {
+            var element = listHtml.get(i);
+            listBook.push({
+                name: element.select(".card-header-title a").text(),
+                cover: element.select(".card-image img").attr("src"),
+                link: element.select(".card-header-title a").attr("href"),
+                host: BASE_URL
+            });
+        }
+        return Response.success(listBook);
+    }
+    return Response.error("Lỗi tìm kiếm");
+}
+
+function getDetail(url) {
+    var response = Fetch.get(url);
+    if (response.status === 200) {
+        var doc = Html.parse(response.body);
+        var chapters = [];
+        var readLink = doc.select("a.button.is-primary").attr("href");
+        if (readLink) {
+            chapters.push({
+                name: "Chương 1",
+                link: readLink,
+                host: BASE_URL
+            });
+        }
+        return Response.success({
+            name: doc.select("h1.title").text(),
+            cover: doc.select(".image-container img").attr("src"),
+            author: doc.select("table.table").text() || "Unknown",
+            description: "Mô tả truyện",
+            chapters: chapters
+        });
+    }
+    return Response.error("Lỗi lấy chi tiết");
+}
+
+function getChap(url) {
+    var response = Fetch.get(url);
+    if (response.status === 200) {
+        var images = [];
+        var doc = Html.parse(response.body);
+        var imgElements = doc.select(".reader-image img");
+        
+        for (var i = 0; i < imgElements.size(); i++) {
+            var img = imgElements.get(i);
+            var imgUrl = img.attr("data-src") || img.attr("src");
+            if (imgUrl) images.push(imgUrl);
+        }
+        return Response.success(images);
+    }
+    return Response.error("Lỗi tải chương");
+}
